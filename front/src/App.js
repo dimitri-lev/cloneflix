3cb831ebc053588e57f1debaf0f28c1fce3ed410
dimitri-lev <dimitri.lev@protonmail.com>
2022-12-26 15:37:17 +0100

    logo

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,15 @@
 import './app.css';
+import logo from './assets/img/logo.png';
 
 function App() {
-  return <header></header>;
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="logo netflix" />
+      </div>
+      <div className="login-container"></div>
+    </header>
+  );
 }
 
 export default App;
