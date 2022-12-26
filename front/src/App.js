87e254f89e52d5b81ad0372541087cdee8f16adb
dimitri-lev <dimitri.lev@protonmail.com>
2022-12-26 22:40:54 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,14 @@ function App() {
         <img src={logo} alt="logo netflix" />
       </div>
       <div className="login-container"></div>
+      <div className="login">
+        <h1>S'identifier</h1>
+        <form action="#">
+          <input type="text" placeholder="E-mail ou numéro de téléphone" />
+          <input type="text" placeholder="Mot de passe" />
+          <input type="submit" value="S'identifier" />
+        </form>
+      </div>
     </header>
   );
 }
