6a50c943932e941e1fed755dd920efa8f6f409cd
dimitri-lev <dimitri.lev@protonmail.com>
2022-12-27 09:21:36 +0100

    login

--- front/src/App.js
+++ front/src/App.js
@@ -7,14 +7,15 @@ function App() {
       <div className="logo">
         <img src={logo} alt="logo netflix" />
       </div>
-      <div className="login-container"></div>
-      <div className="login">
-        <h1>S'identifier</h1>
-        <form action="#">
-          <input type="text" placeholder="E-mail ou numéro de téléphone" />
-          <input type="text" placeholder="Mot de passe" />
-          <input type="submit" value="S'identifier" />
-        </form>
+      <div className="login-container">
+        <div className="login">
+          <h1>S'identifier</h1>
+          <form action="#">
+            <input type="text" placeholder="E-mail ou numéro de téléphone" />
+            <input type="text" placeholder="Mot de passe" />
+            <input type="submit" value="S'identifier" />
+          </form>
+        </div>
       </div>
     </header>
   );
